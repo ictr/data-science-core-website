702680d83ea879002d180a0a659653b8d6d1b5a0
Omid Jafari <omid.jafari@bcm.edu>
2024-05-10 09:48:02 -0500

    Blank NextJS project

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/data-science-core-website",
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
