3e638f89350b87d940f97c69c22bd9b89ab41515
Bo Peng <ben.bob@gmail.com>
2024-05-31 17:16:56 -0500

    change link to entire tile for projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,25 +17,17 @@ export default function ProjectsComp({ projectsData }: ProjectsProps) {
     <div className="container mx-auto">
       <div className="grid gap-6 w-full px-10 mt-10 md:grid-cols-2 xl:grid-cols-4">
         {projectsData.map((proj, index) => (
-          <div
-            className="flex flex-col justify-center px-8 mx-6 my-12 text-center rounded-md shadow-md bg-white text-gray-800"
-            key={ index }
-          >
-            <Image className="self-center flex-shrink-0 -mt-12 bg-center bg-cover bg-gray-500 h-24 w-72 shadow-md" src={ proj.imageUrl } alt="" height={ 96 } width={ 288 } />
-            <div className="flex-1 my-4">
-              <p className="text-xl font-bold">{ proj.name }</p>
-              <p className="text-md text-left">{ proj.description }</p>
+          <Link href={proj.link} target="blank">
+            <div className="flex flex-col justify-center px-8 mx-6 my-12 text-center rounded-md shadow-md bg-white text-gray-800"
+              key={index}>
+              <Image className="self-center flex-shrink-0 -mt-12 bg-center bg-cover bg-gray-500 h-24 w-72 shadow-md" src={
+                proj.imageUrl} alt="" height={96} width={288} />
+              <div className="flex-1 my-4">
+                <p className="text-xl font-bold">{proj.name}</p>
+                <p className="text-md text-left">{proj.description}</p>
+              </div>
             </div>
-            <div className="flex items-center justify-center p-3 space-x-3 border-t-2">
-              <Link
-                href={ proj.link }
-                target="blank"
-                className="bg-blue-800 flex items-center justify-center shadow rounded-full h-8 w-8 text-white"
-              >
-                <FaGlobe />
-              </Link>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
