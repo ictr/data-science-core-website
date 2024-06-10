327816ccabf2cde14b610ed6e81f229bb3cb0f9c
Bo Peng <bo.peng@bcm.edu>
2024-06-10 12:31:36 -0500

    Add BCM logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,12 @@ export default function HeaderComp() {
   return (
     <header className="bg-customBlue text-white p-4">
       <nav className="container mx-auto flex justify-between">
-        <div className="text-xl">Data Science Core</div>
+        <a class="logo" href="https://bcm.edu"></a>
+        <div className="main-title text-xl">Data Science Core</div>
         <div className="space-x-4">
           <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
           <Link href="/projects">Projects</Link>
+          <Link href="/about">About</Link>
         </div>
       </nav>
     </header>
