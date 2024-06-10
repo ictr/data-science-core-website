91b4b303f87c8fae2f220b57be8d357558b10b10
Bo Peng <bo.peng@bcm.edu>
2024-06-10 12:33:48 -0500

    Fix class name

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function HeaderComp() {
   return (
     <header className="bg-customBlue text-white p-4">
       <nav className="container mx-auto flex justify-between">
-        <a class="logo" href="https://bcm.edu"></a>
+        <a className="logo" href="https://bcm.edu"></a>
         <div className="main-title text-xl">Data Science Core</div>
         <div className="space-x-4">
           <Link href="/">Home</Link>
