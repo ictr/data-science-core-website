d116a3d62cb339fd18fc5da431b5c4f056b22ae7
Bo Peng <bo.peng@bcm.edu>
2024-10-23 14:45:41 -0500

    Rename title

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <>
       <div className="text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Services</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Projects and Services</h2>
       </div>
       <ProjectsComp projectsData={ projectsData } />
     </>
