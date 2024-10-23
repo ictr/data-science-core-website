9a8e3cb0ba82feeed539b0724bea5b1eabb3857a
Bo Peng <bo.peng@bcm.edu>
2024-10-23 13:25:02 -0500

    Add RES to projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,4 +1,5 @@
 import nlp_web_interface_screenshot from "@/public/nlp_web_interface_screenshot.png";
+import res_web_interface_screenshot from "@/public/res_web_interface_screenshot.png";
 
 export const projectsData = [
   {
@@ -7,5 +8,12 @@ export const projectsData = [
     description: "An end-to-end Natural Language Processing platform providing annotation and model training tools.",
     imageUrl: nlp_web_interface_screenshot,
     link: "https://ictr.dldcc.bcm.edu/nlp/",
+  },
+  {
+    name: "Research and Engineering Studio",
+    intranetOnly: true,
+    description: "An platform that allows BCM users to start and stop desktop/servers with customized software images and AWS instance types.",
+    imageUrl: res_web_interface_screenshot,
+    link: "https://res.ictr-bcm.org/",
   },
 ];
